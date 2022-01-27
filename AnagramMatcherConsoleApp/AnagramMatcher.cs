a9be7bbe7027dee046848956f3de1bf5c72cd7a9
Jacob <jacob_timms@live.com>
2022-01-27 19:24:25 +0000

    console app built

---
+++ AnagramMatcherConsoleApp/AnagramMatcher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnagramMatcherConsoleApp
+{
+    class AnagramMatcher
+    {
+        public AnagramMatcher()
+        {
+        }
+
+        public void Match(string inputString)
+        {
+            var wordDict = new Dictionary<string, List<string>>();
+            var wordList = ParseInput(inputString).OrderBy(x => x);
+
+            foreach (string word in wordList)
+            {
+                string wordKey = String.Join(',', word.ToCharArray().OrderBy(x => x).ToArray());
+
+                if(wordDict.ContainsKey(wordKey))
+                {
+                    wordDict[wordKey].Add(word);
+                }
+                else
+                {
+                    wordDict.Add(wordKey, new List<string> { word });
+                }
+            }
+            Print(wordDict);
+        }
+
+        private List<string> ParseInput(string input)
+        {
+            string[] removeFileEnd = input.ToLower().Split(".");
+
+            string[] wordArray = removeFileEnd[0].Split("_");
+            List<string> wordList = new List<string>();
+
+            foreach (string word in wordArray)
+            {
+                wordList.Add(word);
+            }
+
+            return wordList;
+        }
+
+        private void Print(Dictionary<string, List<string>> matched)
+        {
+            foreach (List<string> match in matched.Values)
+            {
+                foreach (string word in match)
+                {
+                    Console.Write(word + " ");
+                }
+                Console.WriteLine(Environment.NewLine);
+            }
+        }
+
+    }
+}
---
+++ AnagramMatcherConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnagramMatcherConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string inputString = "apple_car_cider_tar_itch_rat_cried_helicopter_arc.txt";
+
+            AnagramMatcher anagramMatcher = new AnagramMatcher();
+            Console.WriteLine("\nTo match anagrams, enter filename in the following format:" +
+                "\napple_car_cider_tar_itch_rat_cried_helicopter_arc.txt" +
+                "\n\nEnter 'EXIT' to close the program");
+
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+
+                //EXIT COMMAND
+                if (userInput.Equals("EXIT"))
+                {
+                    return;
+                }
+                //MATCH ANAGRAMS COMMAND
+                if(userInput.Length > 1)
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    anagramMatcher.Match(userInput);
+                    Console.WriteLine("Enter a new filename to check another, enter 'EXIT' to exit");
+                }
+            }
+        }
+    }
+}
